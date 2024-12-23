c0ae535f5da21a9a90c2a4b30232dbdf0af518a7
becchi <mebiusu1968@gmail.com>
2024-12-23 19:06:36 +0900

    #5 add function

--- adapter/src/database/model/book.rs
+++ adapter/src/database/model/book.rs
@@ -1,7 +1,8 @@
+use chrono::{DateTime, Utc};
 use kernel::model::{
-    book::Book, 
-    id::{BookId, UserId},
-    user::BookOwner,
+    book::{Book, Checkout}, 
+    id::{BookId, CheckoutId, UserId},
+    user::{BookOwner, CheckoutUser, User},
 };
 
 pub struct BookRow {
@@ -15,8 +16,8 @@ pub struct BookRow {
     pub owner_name: String,
 }
 
-impl From<BookRow> for Book {
-    fn from(value: BookRow) -> Self {
+impl BookRow {
+    pub fn into_book(self, checkout: Option<Checkout>) -> Book {
         // パターンマッチを用いて、`BookRow`の中身を取り出す
         let BookRow {
             book_id,
@@ -26,9 +27,9 @@ impl From<BookRow> for Book {
             description,
             owned_by,
             owner_name,
-        } = value;
+        } = self;
         
-        Self {
+        Book {
             id: book_id,
             title,
             author,
@@ -37,7 +38,38 @@ impl From<BookRow> for Book {
             owner: BookOwner{
                 id: owned_by,
                 name: owner_name,
-            }
+            },
+            checkout,
+        }
+    }
+}
+
+// 貸出情報を格納する方を新規追加　
+pub struct BookCheckoutRow {
+    pub checkout_id: CheckoutId,
+    pub book_id: BookId,
+    pub user_id: UserId,
+    pub user_name: String,
+    pub checked_out_at: DateTime<Utc>,
+}
+
+impl From<BookCheckoutRow> for Checkout {
+    fn from(value: BookCheckoutRow) -> Self{
+        let BookCheckoutRow{
+            checkout_id,
+            book_id,
+            user_id,
+            user_name,
+            checked_out_at,
+        } = value;
+
+        Checkout {
+            checkout_id,
+            checked_out_by: CheckoutUser{
+                id: user_id,
+                name: user_name,
+            },
+            checked_out_at,
         }
     }
 }
